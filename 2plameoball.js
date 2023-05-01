71e4e7d75097ac09cbe4f43ff57c7f67120bd5e2
brycerichards13 <brycerichards13@gmail.com>
2023-05-01 16:39:56 -0500

    Added 2p mode with goals, no new code saved

---
+++ 2plameoball.js
@@ -0,0 +1,12 @@
+// Things that need to be coded
+
+// Things to do before start on 2 player, change the game over logic to fill the board then show game over
+
+// Change main to reassign board when they want to play two player mode
+
+// Superball board with functions
+// Note: Main will have to be changed to reassign the board Object to be a 2 player board Object. That will allow the main.js 
+// buttons to call functions from the two player mode object instead of the one player mode object. It will also allow us
+// to not have to remake all the buttons, and instead just have them call the different functions.
+
+
